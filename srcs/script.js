7313c79d754a415e48907a6441ef8212a72affdc
adriana-codes <helloadrianadiaz@gmail.com>
2024-09-19 16:06:08 +0200

    Created new JS functions

--- srcs/script.js
+++ srcs/script.js
@@ -1,10 +1,17 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#current-value");
   let city = document.querySelector("#current-city");
+  let condition = document.querySelector("#weatherDescription");
+  /*let conditionIcon = document.querySelector("#temperature-icon");*/
+  let humidity = document.querySelector("#humidityValue");
+  let wind = document.querySelector("#windValue");
 
   city.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  console.log(response.data.temperature.current);
+  condition.innerHTML = response.data.condition.description;
+  /*conditionIcon.innerHTML = response.data.condition.icon;*/
+  humidity.innerHTML = response.data.temperature.humidity;
+  wind.innerHTML = response.data.wind.speed;
 }
 
 function searchCity(city) {
@@ -20,6 +27,32 @@ function searchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+let currentDate = new Date();
+
+let dateChange = document.querySelector("#currentDayTime");
+
+let hour = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let weekday = weekdays[currentDate.getDay()];
+
+dateChange.innerHTML = `${weekday} ${hour}:${minutes}`;
 
 let searchCityElement = document.querySelector("#search-form");
 searchCityElement.addEventListener("submit", searchSubmit);
